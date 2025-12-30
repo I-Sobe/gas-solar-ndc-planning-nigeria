41b5698903e241305cc909608834cd950d7990fb
I-Sobe <nwangenesobeolisa@gmail.com>
2025-12-30 12:07:37 +0100

    modify: load carbon price data

--- src/stochastic.py
+++ src/stochastic.py
@@ -10,3 +10,32 @@ Functions:
     run_stochastic_optimization(base_model, samples)
     compute_risk_metrics(results)
 """
+"""
+stochastic.py
+Carbon price stochastic modeling
+"""
+
+import numpy as np
+import pandas as pd
+from src.utils import validate_non_negative
+
+
+def load_carbon_price_data(filepath):
+    """
+    Load cleaned carbon price reference data.
+
+    Parameters
+    ----------
+    filepath : str or Path
+
+    Returns
+    -------
+    pd.DataFrame
+    """
+    df = pd.read_csv(filepath)
+    df = df.dropna(subset=["carbon_price"])
+    validate_non_negative(df["carbon_price"], "carbon_price")
+    return df
+
+
+
